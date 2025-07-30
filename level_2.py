33c4480c876b562528d902b7d28105da558a86a7
ckohl10 <carson.kohlbrenner@gmail.com>
2025-07-30 12:49:58 -0600

    Fixed urdfs

--- level_2.py
+++ level_2.py
@@ -19,15 +19,6 @@ class Robot:
         self.base_frame_id = f"{name}_base"
         self.base_translation = base_translation
 
-    def controller(self):
-        """
-        The controller returns a list of joint values. This can be mapped to a control interface but a simple oscillation is demoed here.
-        """
-        angle = np.sin(time.time()/4)
-        q =np.array([angle, angle, 0.0, angle/2 - np.pi/2, 0.0, angle/2 + np.pi/2, angle, 0.0, 0.0])
-
-        return q
-
     def update(self, q):
         pin.forwardKinematics(self.model, self.data, q)
         pin.updateFramePlacements(self.model, self.data)
@@ -67,7 +58,11 @@ class Robot:
                 T_current = self.data.oMf[frame_id]
                 parent_frame_name = f"{self.name}_link{parent_link_id}"
                 parent_frame_id_in_model = self.model.getFrameId(parent_frame_name)
-                T_parent = self.data.oMf[parent_frame_id_in_model]
+                try:
+                    T_parent = self.data.oMf[parent_frame_id_in_model]
+                except:
+                    print(f"Parent frame {parent_frame_name} not found in model")
+                    continue
                 
                 # Calculate relative transform from parent to current
                 T_joint = T_parent.inverse() * T_current
@@ -94,6 +89,21 @@ class Robot:
             FrameTransforms(transforms=transforms)
         )
 
+def franka_controller():
+    """
+    The controller returns a list of joint values. This can be mapped to a control interface but a simple oscillation is demoed here.
+    """
+    angle = np.sin(time.time()/4)
+    q =np.array([angle, angle, 0.0, angle/2 - np.pi/2, 0.0, angle/2 + np.pi/2, angle, 0.0, 0.0])
+
+    return q
+
+def ur5_controller():
+    angle = np.sin(time.time()/4)
+    q =np.array([angle, angle, 0.0, angle/2 - np.pi/2, 0.0, angle/2 + np.pi/2])
+
+    return q
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Level 2 - Using Frame Transforms')
     parser.add_argument('-w', '--write', type=str, default=None, help='MCAP save location')
@@ -105,14 +115,14 @@ if __name__ == "__main__":
     if args.write is not None:
         writer = foxglove.open_mcap(args.write, allow_overwrite=True)
 
-    panda = Robot("urdf/fp3_1.urdf", "fp3_1", [0.0, 0.0, 0.0])
-    panda_2 = Robot("urdf/fp3_2.urdf", "fp3_2", [0.0, 1.0, 0.0])
+    franka = Robot("urdf/fp3.urdf", "fp3", [0.0, 0.0, 0.0])
+    ur5 = Robot("urdf/ur5.urdf", "ur5", [0.0, 1.0, 0.0])
 
     while True:
-        joint_values = panda.controller()
-        joint_values_2 = panda_2.controller()
+        joint_values = franka_controller()
+        joint_values_2 = ur5_controller()
 
-        panda.update(joint_values)
-        panda_2.update(joint_values_2)
+        franka.update(joint_values)
+        ur5.update(joint_values_2)
         
         time.sleep(0.01)
--- level_3.py
+++ level_3.py
@@ -6,6 +6,8 @@ import time
 import os
 import numpy as np
 import pinocchio as pin
+from transforms3d.quaternions import mat2quat
+from foxglove import Channel
 from foxglove.channels import RawImageChannel, CameraCalibrationChannel
 from foxglove.schemas import RawImage, Timestamp, FrameTransforms, FrameTransform, Vector3, Quaternion, CameraCalibration
 
@@ -20,6 +22,9 @@ class Robot:
         self.base_frame_id = f"{name}_base"
         self.base_translation = base_translation
 
+        # Joint torque channel (optional)
+        self.torque_channel = Channel("/joint_torques", message_encoding="json")
+
     def controller(self):
         """
         The controller returns a list of joint values. This can be mapped to a control interface but a simple oscillation is demoed here.
@@ -29,12 +34,15 @@ class Robot:
 
         return q
 
-    def update(self, q, timestamp=None):
-        if timestamp is None:
-            timestamp = Timestamp(
-                sec=int(time.time()),
-                nsec=int((time.time() - int(time.time())) * 1e9),
-            )
+    def update(self, q, timestamp_ms=None):
+        if timestamp_ms is None:
+            timestamp_ms = int(time.time()*1e3)
+        
+        # Always create the timestamp object
+        timestamp = Timestamp(
+            sec=int(timestamp_ms // 1e3),
+            nsec=int((timestamp_ms % 1e3) * 1e6),
+        )
 
         pin.forwardKinematics(self.model, self.data, q)
         pin.updateFramePlacements(self.model, self.data)
@@ -100,13 +108,39 @@ class Robot:
 
         foxglove.log(
             "/tf",
-            FrameTransforms(transforms=transforms)
+            FrameTransforms(transforms=transforms), 
+            log_time=int(timestamp_ms*1e6)
         )
 
-    def preload_joint_data(self, joint_data, serial_number):
+    def update_torque(self, joint_torques, timestamp_ms=None):
+        if timestamp_ms is None:
+            timestamp_ms = int(time.time()*1e3)
+        
+        torques = {
+            "timestamp": timestamp_ms,
+            "torques": joint_torques.tolist()
+        }
+        self.torque_channel.log(torques, log_time=int(timestamp_ms*1e6))
+
+    def preload_joint_data(self, transforms_path, num_links):
+
+        joint_data = np.load(f"{transforms_path}/joint.npy", allow_pickle=True).item()
+        gripper_data = np.load(f"{transforms_path}/gripper.npy", allow_pickle=True).item()
+        # Get the first serial number's data only
+        first_serial = list(joint_data.keys())[0]
+        joint_data = joint_data[first_serial]
+        gripper_data = gripper_data[first_serial]
+
         # Joint angles, {serial number: {timestamp: joint angle array}}
-        for timestamp, joint_angles in joint_data[serial_number].items():
-            self.update(joint_angles, timestamp)
+        for timestamp, data in joint_data.items():
+            joint_angles = data[:num_links]
+            joint_torques = data[num_links:] # RH20T split
+            gripper_width = gripper_data[timestamp]
+            gripper_width = gripper_width["gripper_info"][0] / gripper_width["gripper_info"][1] #[0-1] scaled to the range of the gripper
+            self.update(np.concatenate((joint_angles[:7], [gripper_width, gripper_width])), timestamp)
+            self.update_torque(joint_torques, timestamp)
+
+        print(f"Preloaded joint data for {self.name}")
                 
 
         
@@ -141,6 +175,7 @@ class PreloadedCamera:
         if frame is None:
             return None
         
+        # Convert milliseconds to seconds and nanoseconds
         sec = int(timestamp // 1e3)
         nsec = int((timestamp % 1e3) * 1e6)
         
@@ -156,32 +191,67 @@ class PreloadedCamera:
             step=self.frame_width*3,
             data=frame.tobytes(),
         )
-        return img_msg, timestamp
+        return img_msg
 
     def load_imgs(self):
         for img_file in os.listdir(self.img_dir):
-            timestamp = int(img_file.split(".")[0])
-            img_msg, timestamp = self.load_frame(timestamp)
-            if img_msg is not None:
-                self.channel.log(img_msg, log_time=timestamp)
+            timestamp_ms = int(img_file.split(".")[0])
+            try:
+                img_msg = self.load_frame(timestamp_ms)
+                log_time_ns = int(timestamp_ms * 1e6)
+                self.channel.log(img_msg, log_time=log_time_ns) # Image message
+                camera_info, camera_transform = self.get_camera_info(timestamp_ms)
+                self.calibration_channel.log(camera_info, log_time=log_time_ns) #Camera info message
+                foxglove.log("/tf", FrameTransforms(transforms=[camera_transform]), log_time=log_time_ns) #Camera transform message
+            except Exception as e:
+                print(f"Error loading frame {timestamp_ms}: {e}")
 
     def calibrate(self):
         intrinsics = np.load(self.calib_dir + "/intrinsics.npy", allow_pickle=True)
         extrinsics = np.load(self.calib_dir + "/extrinsics.npy", allow_pickle=True)
 
         # Intrinsic Calibration
-        P = intrinsics.item().get(self.serial_num)
-        K = P[:3, :3]
+        self.P = intrinsics.item().get(self.serial_num)
+        self.K = self.P[:3, :3]
 
         # Extrinsic Calibration
-        T_cam_world = extrinsics.item().get(self.serial_num)
+        self.T_cam_world = np.array(extrinsics.item().get(self.serial_num), dtype=float)
 
-        # Publish the calibration data
-        self.calibration_channel.log(CameraCalibration(
-            camera_name=self.name,
-            camera_intrinsics=K,
-            camera_extrinsics=T_cam_world,
-        ))
+    def get_camera_info(self, timestamp):
+        camera_info = CameraCalibration(
+            timestamp=Timestamp(
+                sec=int(timestamp // 1e3),
+                nsec=int((timestamp % 1e3) * 1e6),
+            ),
+            frame_id=self.name,
+            width=self.frame_width,
+            height=self.frame_height,
+            # distortion_model="plumb_bob",
+            # D=np.array([0.0, 0.0, 0.0, 0.0, 0.0]),
+            K=self.K.flatten(),
+            P=self.P.flatten(),
+        )
+
+        if self.T_cam_world.ndim == 3:
+            transform_matrix = self.T_cam_world[0]
+        else:
+            transform_matrix = self.T_cam_world
+            
+        translation = transform_matrix[:3, 3]
+        rotation_matrix = transform_matrix[:3, :3]
+        q = mat2quat(rotation_matrix)
+
+        camera_transform = FrameTransform(
+            timestamp=Timestamp(
+                sec=int(timestamp // 1e3),
+                nsec=int((timestamp % 1e3) * 1e6),
+            ),
+            parent_frame_id=WORLD_FRAME_ID,
+            child_frame_id=self.name,
+            translation=Vector3(x=float(translation[0]), y=float(translation[1]), z=float(translation[2])),
+            rotation=Quaternion(x=float(q[1]), y=float(q[2]), z=float(q[3]), w=float(q[0])),
+        )
+        return camera_info, camera_transform
 
     def close(self):
         self.cam.release()
@@ -201,7 +271,8 @@ if __name__ == "__main__":
 
     writer = foxglove.open_mcap(args.task_name+"/data.mcap", allow_overwrite=True)
 
-    cam_SN = np.load(f"data/{robot_name}/calib/devices.npy", allow_pickle=True)
-    cams = [PreloadedCamera(cam_SN[i], f"data/{robot_name}/calib", args.task_name) for i in range(len(cam_SN)-1)]
+    # cam_SN = np.load(f"data/{robot_name}/calib/devices.npy", allow_pickle=True)
+    # cams = [PreloadedCamera(cam_SN[i], f"data/{robot_name}/calib", args.task_name) for i in range(len(cam_SN)-1)]
 
     panda = Robot("urdf/fp3.urdf", "fp3", [0.0, 0.0, 0.0])
+    panda.preload_joint_data(f"{args.task_name}/transformed", 7)
